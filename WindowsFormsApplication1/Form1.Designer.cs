51ad503944207c463eb9dbe582c783c0696551a1
joyharr162 <joyharr162@outlook.com>
2017-01-19 21:39:26 -0500

    created rectangle objects

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(25, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(812, 163);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1031, 735);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Sytem.Threadin;
+using System.Threading;
+using System.Diagnostics;
 
 namespace WindowsFormsApplication1
 {
@@ -16,6 +17,16 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+            Graphics fg = this.CreateGraphics();
+            SolidBrush brush = new SolidBrush (Color.Red);
+            fg.FillEllipse(brush,50, 50, 200, 300);
+            Rectangle r1 = new Rectangle (100, 100, 50, 50);
+            Rectangle r2 = new Rectangle (50, 50, 50, 50);
+            if (r1.IntersectsWith(r2))
+                MessageBox.Show("Intersected!");
+            else
+                MessageBox.Show("Non-Intersected!");
+
         }
     }
 }
